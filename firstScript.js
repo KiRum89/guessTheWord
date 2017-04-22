b96dcbc14974e4942042396629624bd07cfd584b
KirillPlasma <funduk89@gmail.com>
2017-04-23 00:40:40 +0200

    learning

--- firstScript.js
+++ firstScript.js
@@ -1,1 +1,9 @@
+document.body.style.border = "5px solid red";
+var h=document.createTextNode('Hello World');
+document.body.appendChild(h);
 
+
+
+for (i=0;i<document.body.childNodes.length;++i){
+	alert(document.body.childNodes[i].textContent);
+}
